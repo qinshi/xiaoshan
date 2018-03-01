766ea179eefcfe0d32940c572126b89ee3c89983
lishiqi <lishiqi@zhihu.com>
2018-03-01 18:51:21 +0800

    stash

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,13 +3,21 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    messages: [{'speaker': 'i', 'msg': 'test'}],
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
+    // const setData = response => this.setData({log: response})
+    // util.ask("你好", setData);
+    this.siriSpeech("hello");
+    this.iSpeech("我是小明");
+  },
+  iSpeech: function(msg) {
+    this.data.messages.push({ 'speaker': 'i', 'msg': msg });
+    this.setData({'messgaes': this.data.messages});
+  },
+  siriSpeech: function(msg) {
+    this.data.messages.push({ 'speaker': 'siri', 'msg': msg }); 
+    this.setData({ 'messgaes': this.data.messages });
+  },
 })
